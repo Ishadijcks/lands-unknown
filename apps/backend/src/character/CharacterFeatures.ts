b71c12d3947203821fde1b7ed30ac97d0e988fb9
Isha Dijcks <isha@q42.nl>
2023-05-29 16:43:22 +0200

    refactor(LandsUnknown): Refactor activity scheduling to work on locations

--- apps/backend/src/character/CharacterFeatures.ts
+++ apps/backend/src/character/CharacterFeatures.ts
@@ -1,9 +1,11 @@
 import type { CharacterSkills } from "backend/character/skills/CharacterSkills";
 import type { CharacterActivityQueue } from "backend/character/activities/CharacterActivityQueue";
 import type { CharacterInventory } from "backend/character/inventory/CharacterInventory";
+import { CharacterWorldMap } from "backend/character/worldmap/CharacterWorldMap";
 
 export interface CharacterFeatures {
   skills: CharacterSkills;
   activityQueue: CharacterActivityQueue;
   inventory: CharacterInventory;
+  worldMap: CharacterWorldMap;
 }
