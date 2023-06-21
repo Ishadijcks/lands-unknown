9ad76e6e50552b3305b8c95ecf5087a659a9b65b
Isha Dijcks <isha@q42.nl>
2023-06-21 10:49:57 +0200

    fix: Re-add json schema generation

--- packages/common/scripts/content-validate.ts
+++ packages/common/scripts/content-validate.ts
@@ -1,7 +1,12 @@
 import { ContentFlattener } from "common/scripts/ContentFlattener";
+import { SchemaGenerator } from "common/scripts/SchemaGenerator";
+import { SkillDefinitionSchema } from "common/parsers/skill/SkillDefinitionSchema";
 
 // Such that we can validate all content with Zod
 const flattener = new ContentFlattener();
 flattener.parseAllYamlFiles(true);
 
 // And generate JSON schemas to improve the developer experience
+const generator = new SchemaGenerator();
+generator.addSchema({ schema: SkillDefinitionSchema, schemaName: "skill" });
+generator.write();
