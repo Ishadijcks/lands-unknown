d5ef11aabb8b70194e3a01f0a8124da64c3db38b
Isha Dijcks <ishadijcks@gmail.com>
2023-07-04 21:14:31 +0200

    Add activities display

---
+++ apps/frontend/src/routes/docs/activities/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { gameData } from "common/content/GameData";
+  import type {ActivityDetail} from "common/game/activities/ActivityDetail";
+
+  const activities: ActivityDetail[] = Object.values(gameData.activityDetailMap);
+</script>
+
+<div class="flex flex-col w-full items-center">
+  <p class="font-bold text-2xl">Activity Overview</p>
+  <br />
+  <div class="flex flex-row flex-wrap gap-1 justify-center">
+    {#each activities as activity}
+      <a href="/docs{activity.hrid}">
+        <span>{JSON.stringify(activity)}</span>
+      </a>
+    {/each}
+  </div>
+</div>
---
+++ apps/frontend/src/routes/docs/activities/[...activity]/+page.ts
@@ -0,0 +1,15 @@
+import { error } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
+import { gameData } from "common/content/GameData";
+import type {ActivityHrid} from "common/game/activities/ActivityHrid";
+
+export const load = (({ params }) => {
+  const hrid = ("/activities/" + params.activity) as ActivityHrid;
+  const content = gameData.activityDetailMap[hrid];
+  if (!content) {
+    throw error(404, "Not found");
+  }
+  return {
+    detail: content,
+  };
+}) satisfies PageLoad;
